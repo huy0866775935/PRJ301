90e9254ccc31f9c599de9bc1c1e4ea7f0be2da9b
huy0866775935 <134285643+huy0866775935@users.noreply.github.com>
2023-09-28 09:32:08 +0700

    Reading document

--- Assignment_PRJ301/src/java/myserverlet/severlet.java
+++ Assignment_PRJ301/src/java/myserverlet/severlet.java
@@ -36,7 +36,7 @@ public class severlet extends HttpServlet {
             out.println("<title>Servlet severlet</title>");  
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet severlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet severlet at " + request.getContextPath () +"</h1>");
             out.println("</body>");
             out.println("</html>");
         }
